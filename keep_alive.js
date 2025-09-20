a5dace64ade6313dab52a6bcc09d1172545f171f
harshsinha003 <harshvardhansinha88@gmail.com>
2025-09-21 03:05:46 +0530

    Add keep_alive.js to keep backend active

---
+++ keep_alive.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+const url = `https://house-price-predictor-fouz.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
